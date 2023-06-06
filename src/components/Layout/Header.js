30a100b5b20fa7731b7dbfff2a8779cba8148a66
Aman <Sharmaamqn@gmail.com>
2023-06-06 17:19:31 +0530

    -add cart button in header component -add components to display meal items

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,20 @@
 import { Fragment } from "react";
 import styles from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
-  <Fragment>
-    <header className={styles.header}>
-      <h1>React Meals</h1>
-      <button>Cart</button>
-    </header>
-    <div className={styles["main-image"]}>
-      <img src={mealsImage} alt="A table full of delicious food!" />
-    </div>
-  </Fragment>;
+  return (
+    <Fragment>
+      <header className={styles.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
